6d1aceb85031b70ef7eee60415a4425bf9b26e81
Ryan-JW-Kim <rj.kim.toronto@gmail.com>
2022-12-08 01:04:34 -0500

    Sync code that cannot run on WSL but should tested on Ubuntu

--- display.py
+++ display.py
@@ -1,8 +1,36 @@
-
+from PIL import Image
+import matplotlib.pyplot as plt
+import matplotlib.image as img
 
 class Display:
     def __init__(self, path, as_pixels=False):
-        pass
+        self.path = path
+
+        if as_pixels:
+            self.mode = "Pixel Grid"
+            self.grid_size = (5,5) # Width, Height ratio per frame
+
+    def pixel_grid_generator(self):
+
+        im = img.imread(self.path)
+        # pixels = list(img.getdata())
+        # width, height = img.size
+
+        yield im
 
     def launch(self):
-        pass
+        
+        # fig = plt.figure()
+        # ax = fig.add_subplot(111)
+        # plt.ion()
+        # plt.show()
+
+        if self.mode == "Pixel Grid":
+            for frame in self.pixel_grid_generator():
+                # img = ax.imshow(frame)
+                # plt.draw()
+                plt.imshow(frame)
+    
+    def test(self):
+        testImage = img.imread(self.path)
+        return testImage
--- main.py
+++ main.py
@@ -1,10 +1,28 @@
+import matplotlib.pyplot as plt
 from analysis import Analysis
 from display import Display
 from reader import Reader
 import threading
 
 
-if __name__ == "__main__":
+import matplotlib.pyplot as plt
+
+fig, ax = plt.subplots()
+
+fruits = ['apple', 'blueberry', 'cherry', 'orange']
+counts = [40, 100, 30, 55]
+bar_labels = ['red', 'blue', '_red', 'orange']
+bar_colors = ['tab:red', 'tab:blue', 'tab:red', 'tab:orange']
+
+ax.bar(fruits, counts, label=bar_labels, color=bar_colors)
+
+ax.set_ylabel('fruit supply')
+ax.set_title('Fruit supply by kind and color')
+ax.legend(title='Fruit color')
+
+plt.show()
+
+if __name__ != "__main__":
 
     analytics = Analysis()
     analytics.start_time()
@@ -15,23 +33,26 @@ if __name__ == "__main__":
     display = Display(path_to_image, as_pixels=True)
 
     # Created Read Image object
-    reader = Reader()
+    # reader = Reader()
 
     # Launch Reader thread
-    read_thread = threading.Thread(target=reader.launch)
-    read_thread.start()
+    # read_thread = threading.Thread(target=reader.launch)
+    # read_thread.start()
 
     # Launch Display thread
-    display_thread = threading.Thread(target=display.launch)
-    display_thread.start()
+    # display_thread = threading.Thread(target=display.launch)
+    # display_thread.start()
+    # display.launch()
+    obj = display.test()
+    plt.imshow(obj)
 
     # Wait till threads successfull execute
-    read_thread.join()
-    display_thread.join()
+    # read_thread.join()
+    # display_thread.join()
     analytics.end_time()
 
     # Display Reader Output
-    reader.display_result()
+    # reader.display_result()
 
     # Print input/output comparision
     analytics.report()
